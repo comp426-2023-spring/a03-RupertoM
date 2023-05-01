578b5b8666a995e80d8a60f62be2a5987bec85fd
Ruperto A. Martinez <77865052+RupertoM@users.noreply.github.com>
2023-04-30 21:57:54 -0400

    Help and Rules for RPSLS

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
-
+import {rpsls, RPSLSHelp, RPSLSRules} from "../lib/rpsls.js"
 import minimist from "minimist";
+const args = minimist(process.argv.slice(2),{
+    alias: {
+        r: "rules"
+    }
+});
+
+if (args.h || args.help) {
+    RPSLSHelp();
+    process.exit();
+} else if (args.r || args.rules) {
+    RPSLSRules();
+    process.exit();
+}
